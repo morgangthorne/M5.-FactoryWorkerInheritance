4d2869ac1d672bc352d5613e94d9a27d8aa1cbea
morgan thorne <morgangthorne@outlook.com>
2026-04-16 20:28:30 -0400

    Added test for employee class in main

--- Employee.cpp
+++ Employee.cpp
@@ -3,18 +3,29 @@
 
 using namespace std;
 
+
+
+//Default Constructor
+//Pre: None
+//Post: Initializes members to empty strings
 Employee::Employee() {
 	Name = "";
 	EmployeeNumber = "";
 	HireDate = "";
 }
 
+//Param Constructor
+//Pre: Valid String Provided
+//Post: Initializes all member variables
 Employee::Employee(string n, string Num, string Date) {
 	Name = n;
 	EmployeeNumber = Num;
 	HireDate = Date;
 }
 
+//Setters
+//Pre: Valid string input
+//Post: Updates corresponding member variable
 void Employee::SetName(string n) {
 	Name = n;
 }
@@ -27,6 +38,9 @@ void Employee::SetHireDate(string Date) {
 	HireDate = Date;
 }
 
+//Getters
+//Pre: None
+//Post: Returns member variable
 string Employee::GetName() const {
 	return Name;
 }
@@ -39,6 +53,9 @@ string Employee::GetHireDate() const {
 	return HireDate;
 }
 
+//Print Function
+//Pre: Object is initialized
+//Post: Prints employee
 void Employee::PrintEmployee() const {
 	cout << "Name: " << Name << endl;
 	cout << "Employee Number: " << EmployeeNumber << endl;
--- M5. FactoryWorkerInheritance.cpp
+++ M5. FactoryWorkerInheritance.cpp
@@ -1,9 +1,19 @@
+#include <iostream>
+#include "Employee.h"
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Testing Employee Class
+
+    Employee emp1("Morgan Thorne", "123-A", "01/15/2020");
+
+    cout << "Employee Information: " << endl;
+
+    emp1.PrintEmployee();
 }
 
 
