cbeed0b39e8bee8e326a5d135f5ea4ea72bf5081
morgan thorne <morgangthorne@outlook.com>
2026-05-09 13:52:08 -0400

    Cleaned up code, also added for clarity

--- M5. FactoryWorkerInheritance.cpp
+++ M5. FactoryWorkerInheritance.cpp
@@ -2,6 +2,7 @@
 #include "Employee.h"
 #include "ProductionWorker.h"
 #include "ShiftSupervisor.h"
+#include "TeamLeader.h"
 
 #include <iostream>
 
@@ -9,16 +10,19 @@ using namespace std;
 
 int main()
 {
+
+    //Day Shift object
     ProductionWorker DayWorker("Morgan Thorne", "210", "6/27/2003", 1, 15.60);
-
-    ProductionWorker NightWorker("John Doe", "212", "5/05/2005", 2, 17.38);
-
-    //Day Shift worker info
+    
+    //Prints Day Shift info
     cout << "Day Shift Worker" << endl;
     DayWorker.PrintProductionWorker();
 
     cout << endl;
 
+    //NightSHift object
+    ProductionWorker NightWorker("John Doe", "212", "5/05/2005", 2, 17.38);
+    
     //Nightshift worker info
     cout << "Night Shift Worker" << endl;
     NightWorker.PrintProductionWorker();
@@ -29,6 +33,13 @@ int main()
     //Prints the Supervisors info
     cout << "\nShift Supervisor Info: " << endl;
     Supervisor.PrintShiftSupervisor();
+
+    //TeamLeader Object
+    TeamLeader Leader("Ricky Bobby", "329", "2/10/2000", 1, 23.21, 600.0, 21, 19);
+
+    //Prints TeamLeader info
+    cout << "Team Leader Info: " << endl;
+    Leader.PrintTeamLeader();
 
     return 0; 
 
