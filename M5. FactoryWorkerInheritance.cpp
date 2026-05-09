49c74181b7c0370110c01a6337e011415e734beb
morgan thorne <morgangthorne@outlook.com>
2026-05-09 13:17:08 -0400

    created test for Shift Supervisor in main, output is correct

--- M5. FactoryWorkerInheritance.cpp
+++ M5. FactoryWorkerInheritance.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Employee.h"
 #include "ProductionWorker.h"
+#include "ShiftSupervisor.h"
 
 #include <iostream>
 
@@ -10,15 +11,24 @@ int main()
 {
     ProductionWorker DayWorker("Morgan Thorne", "210", "6/27/2003", 1, 15.60);
 
-    ProductionWorker NightWorker("John Doe", "212", "5/05/2005", 1, 17.38);
+    ProductionWorker NightWorker("John Doe", "212", "5/05/2005", 2, 17.38);
 
+    //Day Shift worker info
     cout << "Day Shift Worker" << endl;
     DayWorker.PrintProductionWorker();
 
     cout << endl;
 
+    //Nightshift worker info
     cout << "Night Shift Worker" << endl;
     NightWorker.PrintProductionWorker();
+
+    //ShiftSupervisor object
+    ShiftSupervisor Supervisor("Bob Bob", "300", "1/16/2003", 72000.00, 3000.00);
+
+    //Prints the Supervisors info
+    cout << "\nShift Supervisor Info: " << endl;
+    Supervisor.PrintShiftSupervisor();
 
     return 0; 
 
