bc0c9abb60c536d54b8d8f7ce001d3f0b561da69
morgan thorne <morgangthorne@outlook.com>
2026-05-09 13:10:55 -0400

    Added ShiftSuperVisor.h with class declarations and member variables, also implemented ShiftSupervisor.cpp with constructs, getters/setters, and a print function

---
+++ ShiftSupervisor.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "ShiftSupervisor.h"
+
+using namespace std;
+
+// Default constructor
+ShiftSupervisor::ShiftSupervisor() : Employee() {
+    AnnualSalary = 0.0;
+    AnnualBonus = 0.0;
+}
+
+// Parameterized constructor
+ShiftSupervisor::ShiftSupervisor(string Name, string Num, string Date, double Salary, double Bonus) : Employee(Name, Num, Date) {
+    AnnualSalary = Salary;
+    AnnualBonus = Bonus;
+}
+
+// Setters
+void ShiftSupervisor::SetAnnualSalary(double Salary) {
+    AnnualSalary = Salary;
+}
+
+void ShiftSupervisor::SetAnnualBonus(double Bonus) {
+    AnnualBonus = Bonus;
+}
+
+// Getters
+double ShiftSupervisor::GetAnnualSalary() const {
+    return AnnualSalary;
+}
+
+double ShiftSupervisor::GetAnnualBonus() const {
+    return AnnualBonus;
+}
+
+// Print function
+void ShiftSupervisor::PrintShiftSupervisor() const {
+    PrintEmployee(); 
+    cout << "Annual Salary: $" << AnnualSalary << endl;
+    cout << "Annual Bonus: $" << AnnualBonus << endl;
+}
